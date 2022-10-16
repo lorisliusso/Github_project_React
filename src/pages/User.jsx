3e5646a3efa9701eafa88308a11c4294aceeae7a
Loris Liusso <lorisliusso@gmail.com>
2022-10-16 12:03:57 +0200

    Bugs fixed

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -57,7 +57,8 @@ function User() {
                     </Link>
                 </div>
 
-                <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+                <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3
+                 md:grid-cols-3 mb-8 md:gap-8'>
                     <div className='custom-card-image mb-6 md:mb-0'>
                         <div className='shadow-xl card '>
                             <figure>
@@ -92,7 +93,7 @@ function User() {
                             </div>
                         </div>
 
-                        <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+                        <div className='w-full rounded-lg shadow-md bg-gray-800 stats'>
                             {location && (
                                 <div className='stat'>
                                     <div className='stat-title text-md'>Location</div>
@@ -127,7 +128,7 @@ function User() {
                     </div>
                 </div>
 
-                <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+                <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-gray-800  stats'>
                     <div className='grid grid-cols-1 md:grid-cols-3'>
                         <div className='stat'>
                             <div className='stat-figure text-secondary'>
